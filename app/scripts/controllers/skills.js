6b1002964104455dea9550c718d86f0b1da9dd05
Garance <vallat.garance@etu.unice.fr>
2015-04-13 13:39:49 +0200

    progress bar for the skills !! :)

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -16,4 +16,28 @@ angular.module('vitrineApp')
     .error(function(error) {
       console.log(error); //pour le debug
     });
+
+    $scope.convertLvl = function(lvlInWord) {
+      var out=0;
+      if (lvlInWord=="perfect") {
+        out=100;
+      }
+      else if (lvlInWord=="excellent") {
+        out=90;
+      }
+      else if (lvlInWord=="good") {
+        out=80;
+      }
+      else if (lvlInWord=="workToDo") {
+        out=50;
+      }
+      else if (lvlInWord=="beginner") {
+        out=30;
+      }
+      else if (lvlInWord=="jacks") {
+        out=10;
+      }
+      return out;
+    };
+
   });
